f9a42b6482b0aea4c306866d46c6de7094e39c13
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-11 12:18:24 +0530

    Add method get_user to return current_user in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  def get_user
+    if user_signed_in?
+      return current_user
+    end
+  end
 end
