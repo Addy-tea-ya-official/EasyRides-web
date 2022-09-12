dd6edffe6001f881c4701e52fbc0bc8953927aa8
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-12 14:24:21 +0530

    Add boarding_time field to response json of index action for Services controller Add http status 200 to index method of Services controller Update show action to book_ride action for Services controller Add rescue block and appropriate http status to book_ride action for Services controller to validate booking of service only if exists in active records Add user validation, appropritae http status to create action of Service Controller for fetching logged in user's vehicle attributes using body params

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -17,26 +17,32 @@ class ServicesController < ApplicationController
           destination: service.destination,
           current_capacity: service.current_capacity,
           fair: service.fair,
+          boarding_time: service.boarding_time,
           service_id: service.id
         }
       )
     end 
-    render json: services_list
+    render json: services_list, status: 200
   end
 
-  def show
+  def book_ride
     user = get_user
-    service = Service.find(params[:id])
-    ticket = ServiceTicket.new(
-      {
-        service_id: service.id,
-        passenger_id: user.id
-      }
-    )
-    if ticket.save
-      render json: ticket  
-    else
-      render status: :unprocessable_entity
+    begin
+      service = Service.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: {message: "Service does not exist"}, status: 204
+    else 
+      ticket = ServiceTicket.new(
+        {
+          service_id: service.id,
+          passenger_id: user.id
+        }
+      )
+      if ticket.save
+        render json: ticket, status: 200  
+      else
+        render json: {message: "Ride not booked"}, status: :unprocessable_entity
+      end
     end
   end
 
@@ -51,23 +57,27 @@ class ServicesController < ApplicationController
   end
 
   def create
-    user = get_user
-    service = Service.new(
-      {
-        vehicle_id: service_params[:vehicle_id],
-        destination: service_params[:destination],
-        current_capacity: Vehicle.find(service_params[:vehicle_id]).capacity - 1,
-        fair: service_params[:fair],
-        boarding_time: service_params[:boarding_time]
-      }
-    )
-    if service.save
-      render json: service 
+    user = get_user 
+    vehicle = Vehicle.where(user_id: user.id, id: service_params[:vehicle_id])
+    if vehicle.empty?
+      render json: {message: "User does not have vehicle"}, status: 400
     else
-      render status: :unprocessable_entity
+      service = Service.new(
+        {
+          vehicle_id: service_params[:vehicle_id],
+          destination: service_params[:destination],
+          current_capacity: vehicle[0].capacity - 1,
+          fair: service_params[:fair],
+          boarding_time: service_params[:boarding_time]
+        }
+      )
+      if service.save
+        render json: service, status: 200 
+      else
+        render json: {message: "Service not created"}, status: :unprocessable_entity
+      end
     end
   end
-
   private
     def service_params
       params.require(:service).permit(:vehicle_id, :destination, :fair, :boarding_time)
