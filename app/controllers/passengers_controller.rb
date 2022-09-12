42bf3176a3682dab54f4bab9ad62f529c82bdef3
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-12 14:37:52 +0530

    Add validation for presence of passenger_ticket to index action of Passengers controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,15 +4,19 @@ class PassengersController < ApplicationController
   def index
     user = get_user
     passenger_ticket = ServiceTicket.where(passenger_id: user.id)
-    passenger_ticket.each do |ticket|
-      vehicle = Vehicle.find(Service.find(ticket.service_id).vehicle_id)
-      render json: {
-        passenger_name: user.name,
-        vehicle_name: vehicle.name,
-        vehicle_registeration_number: vehicle.registeration_number,
-        driver_name: User.find(vehicle.user_id).name,
-        ticket_status: ticket.request_status
-      }
+    if passenger_ticket.empty?
+      render json: {message: "User does not have any ticket"}, status: 204
+    else
+      passenger_ticket.each do |ticket|
+        vehicle = Vehicle.find(Service.find(ticket.service_id).vehicle_id)
+        render json: {
+          passenger_name: user.name,
+          vehicle_name: vehicle.name,
+          vehicle_registeration_number: vehicle.registeration_number,
+          driver_name: User.find(vehicle.user_id).name,
+          ticket_status: ticket.request_status
+        }, status: 200
+      end
     end
   end
 end
