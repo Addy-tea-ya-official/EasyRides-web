8c364180650832a32234cee4e78b4271554190cb
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-15 15:02:56 +0530

    Update the relationship of vehicle to services as has many Add validation for registeration_number for Vehicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,8 @@
 class Vehicle < ApplicationRecord
   #Associations
-  has_one :service
+  has_many :services
   belongs_to :user
+
+  #Validations
+  validates :registeration_number, format: {with: /[A-Z][A-Z]\s\d\d\s[A-Z][A-Z]\s\d\d\d\d/, message:"Enter valid vehilce registeration number"} 
 end
