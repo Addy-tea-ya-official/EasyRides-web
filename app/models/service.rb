81396afce42270e6d01be7eff6579542f0d05d24
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-15 14:55:37 +0530

    Update the relationship with service_tickets to has many Add validation for boarding_time to be later than current time

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,8 +1,16 @@
 class Service < ApplicationRecord
   #Associations
   belongs_to :vehicle
-  has_one :service_ticket
+  has_many :service_tickets
 
   #Validations
   validates :boarding_time, :fair, presence: true
+  validate :valid_boarding_time?
+
+  private
+  def valid_boarding_time?
+    if boarding_time < DateTime.now
+      self.errors.add :boarding_time, 'Boarding time can not be in past'
+    end  
+  end
 end
