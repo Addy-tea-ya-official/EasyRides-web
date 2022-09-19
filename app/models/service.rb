17d1cb562be2de6b853fec50a1f8d15923d763fe
Addy-tea-ya-official <aditya.yadav19@pccoepune.org>
2022-09-19 16:00:25 +0530

    Add validation for valid boarding time and positive fair amount

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,12 +5,22 @@ class Service < ApplicationRecord
 
   #Validations
   validates :boarding_time, :fair, presence: true
-  validate :valid_boarding_time?
+  validate :valid_boarding_time
+  validate :positive_fair_amount
 
   private
-  def valid_boarding_time?
-    if boarding_time < DateTime.now
-      self.errors.add :boarding_time, 'Boarding time can not be in past'
-    end  
+  def valid_boarding_time
+    if boarding_time != nil
+      if boarding_time < DateTime.now
+        self.errors.add :boarding_time, "can't be in past"
+      end  
+    end
+  end
+  def positive_fair_amount
+    if fair != nil
+      if fair < 0
+        self.errors.add :fair, "can't be negative"
+      end  
+    end
   end
 end
