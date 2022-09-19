4671c7af7fb907037fb1a1f2c5b67057e9ad1fc2
Addy-tea-ya-official <aditya.yadav19@pccoepune.org>
2022-09-19 16:26:12 +0530

    Add spec for ticket booking action in Passengers controller

--- spec/acceptance/passengers_spec.rb
+++ spec/acceptance/passengers_spec.rb
@@ -5,10 +5,10 @@ resource "Passengers" do
   let!(:passenger) {User.create!(email: "iop1@gmail.com", password: 123456, password_confirmation: 123456)}
   let!(:driver) {User.create!(email: "jkl1@gmail.com", password: 123456, password_confirmation: 123456)}
   let!(:vehicle) {Vehicle.create!(name: "a", registeration_number: "MH 12 KY 3212", capacity: 4, user_id: driver.id)}
-  let!(:service) {Service.create!(vehicle_id: vehicle.id, destination: "K", current_capacity: 4, fair: 500, boarding_time: DateTime.now + 100)}
-  let!(:service_ticket) {ServiceTicket.create!({service_id: service.id, passenger_id: passenger.id})}
-  # byebug
-  get "/ticket" do
+  let!(:service) {Service.create!(vehicle_id: vehicle.id, destination: "K", current_capacity: 4, fair: 500, boarding_time: DateTime.now + 1)}
+  let(:service_ticket) {ServiceTicket.create!({service_id: service.id, passenger_id: passenger.id})}
+  
+  get "/tickets" do
     context '200' do
       before do
         auth_headers = passenger.create_new_auth_token
@@ -32,7 +32,54 @@ resource "Passengers" do
     end
   end
 
-  get "/ticket" do
+  post "/ticket" do
+    parameter :id, "service id", required: true
+    context '200' do
+      before do 
+        auth_headers = passenger.create_new_auth_token
+        header "Authorization", auth_headers["Authorization"]
+      end
+      example "Passenger booking ride" do
+        request = {
+          service: {
+            id: service.id
+          }
+        }
+        expected_response = {
+          message: "Ticket saved successfully",
+          data:{
+          service_id: service.id,
+          passenger_id: passenger.id
+          }
+        }.to_json
+        do_request(request)
+        expect(status).to eq 200
+        expect(response_body).to eq(expected_response)
+      end
+      context '200' do
+        before do 
+          auth_headers = passenger.create_new_auth_token
+          header "Authorization", auth_headers["Authorization"]
+          ServiceTicket.create!({service_id: service.id, passenger_id: passenger.id})
+        end
+        example "Passenger booking already booked ride" do
+          request = {
+            service: {
+              id: service.id
+            }
+          }
+          expected_response = {
+            message: "Appointment for this service already exists",
+          }.to_json
+          do_request(request)
+          expect(status).to eq 200
+          expect(response_body).to eq(expected_response)
+        end
+      end
+    end
+  end
+
+  get "/tickets" do
     context '204' do
       before do
         ServiceTicket.destroy_all
